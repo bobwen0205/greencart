654ac17fd055949d31a99ad0d0661af9ea0d1a78
bobwen0205 <wenmingzhi10@gmail.com>
2025-04-27 18:01:17 -0400

    third commit

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express from 'express';
+import connectDB from './configs/db.js';
+import 'dotenv/config';
+import userRouter from './routes/userRoute.js';
+import sellerRouter from './routes/sellerRoute.js';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+await connectDB();
+
+// Allow multiple origins
+const allowedOrigins = ['http://localhost:5173'];
+
+// Middleware configuration
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: allowedOrigins, credentials: true }));
+
+
+app.get('/', (req, res) => res.send('API is Working'));
+app.use('/api/user', userRouter);
+app.use('/api/seller', sellerRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+})
